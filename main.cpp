2e1e02a8a6d8645a73886b0e1240901ea172ae50
Jakub Silhan <jakub.silhan@tul.cz>
2025-10-02 11:47:57 +0200

    Ready main

--- main.cpp
+++ main.cpp
@@ -1,39 +1,12 @@
-#include "include/recognizers/FaceRecognizer.hpp"
-#include "include/recognizers/RedRecognizer.hpp"
+#include "include/runners/TrackApp.hpp"
 
 
 int main()
 {
-    // Helloworld
-    //runProject();
+    TrackApp trackApp;
 
-    // Static
-    //static_treshold_search();
-
-    // HSV
-    //runHsv();
-
-    // Red Cup image
-    //RedRecognizer recognizer;
-    //recognizer.run_static();
-
-    // Red Cup video
-    //RedRecognizer recognizer;
-    //recognizer.run_video("resources/video.mkv");
-
-    // Face
-    FaceRecognizer recognizer;
-    //// Initialize classifier and camera
-    if (!recognizer.init()) {
-        std::cerr << "Failed to initialize FaceRecognizer." << std::endl;
-        return -1;
-    }
-
-    //// Run face detection loop
-    int result = recognizer.run();
-    if (result != 0) {
-        std::cerr << "FaceRecognizer encountered an error." << std::endl;
-    }
+    trackApp.init();
+    trackApp.run();
 
     return 0;
 }
