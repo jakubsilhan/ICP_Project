c10b5d03584b35d1ae042f0bd9f7aa3bd09f0e07
hyneq <hynekv.svobodny@gmail.com>
2026-01-31 16:55:04 +0100

    Removed Raster Processing App and the selector

--- source/main.cpp
+++ source/main.cpp
@@ -1,37 +1,12 @@
-#include "include/runners/RasterApp.hpp"
 #include "include/runners/GLApp.hpp"
 #include "include/scenes/ShooterScene.hpp"
 #define MINIAUDIO_IMPLEMENTATION
 #include "audio/Miniaudio.h"
 
-
 int main()
 {
-    std::cout << "==============================" << std::endl;
-    std::cout << "   APPLICATION SELECTOR       " << std::endl;
-    std::cout << "==============================" << std::endl;
-    std::cout << "1 - Raster Processing App" << std::endl;
-    std::cout << "2 - Main OpenGL App" << std::endl;
-    std::cout << "Selection: ";
-
-    char selection;
-    std::cin >> selection;
-
-    switch (selection) {
-    case '1': {
-        RasterApp rastApp;
-        if (rastApp.init()) rastApp.run();
-        break;
-    }
-    case '2': {
-        GLApp glApp;
-        if (glApp.init()) glApp.run();
-        break;
-    }
-    default:
-        std::cout << "Exiting!" << std::endl;
-        break;
-    }
+    GLApp glApp;
+    if (glApp.init()) glApp.run();
 
     return 0;
 }
